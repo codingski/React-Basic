db432347213f70ed0cd4ed36b8d21bcece565eef
Coding_Chan <69722812+codingski@users.noreply.github.com>
2021-01-20 14:32:56 +0900

    state
    state

--- habit-tracker/src/index.js
+++ habit-tracker/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import '@fortawesome/fontawesome-free/js/all.js'; // FontAwesome 프리버전에 있는 모든 것들을 임포트
 
 ReactDOM.render(
   <React.StrictMode>
