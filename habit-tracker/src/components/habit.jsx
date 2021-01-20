6d4f376f9cad860ea4b0c36210401fea5ee72646
Coding_Chan <69722812+codingski@users.noreply.github.com>
2021-01-20 16:42:40 +0900

    2021. 01. 20
    2021. 01. 20

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 
+// Habit은 부모컴포넌트로부터 전달받은 Props로 습관의 데이터를 보여주기만 하는 컴포넌트 이기 때문에
+// 이 컴포넌트 안에 자체적으로 State를 가지고 있을 필요는 전혀 없다.
+
 class Habit extends Component { // 클래스는 항상 대문자로 시작!
-    state = {
-        count: 0,
+    handleIncrement = () => {
+        this.props.onIncrement(this.props.habit);
     };
 
-    handleIncrement = () => {
-        // state 오브젝트 안에 있는 count를 증가 한 뒤 state를 업데이트 해야 함.
-        this.setState({count: this.state.count + 1});
-    }
-
     handleDecrement = () => {
-        const count = this.state.count - 1;
-        this.setState({count: count < 0 ? 0 : count});
-    }
+        this.props.onDecrement(this.props.habit);
+    };
+
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
+    };
 
     render() {
         // const habitName = this.props.habit.name; 이렇게 하는 방법도 있지만 아래 더욱 쉬운방법이 있음!
@@ -27,7 +28,7 @@ class Habit extends Component { // 클래스는 항상 대문자로 시작!
         <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
             <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button className="habit-button habit-delete" onClick={this.handleDelete}>
             <i className="fas fa-trash"></i>
         </button>
         </li>
